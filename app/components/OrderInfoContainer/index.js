806e77985012c69b580132c7e201397ce13c2788
Rakhi Mishra <rakhimishra@TECH-RAKHI.local>
2022-05-24 16:46:09 +0530

    adding changes

--- app/components/OrderInfoContainer/index.js
+++ app/components/OrderInfoContainer/index.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { Container } from "./style";
-import { CheckCircleFilled } from "@ant-design/icons";
-const OrderInfocontainer = () => {
+import { Container, OrderStatus, Icon } from "./style";
+import { CheckOrderStatus, Color, ExpectedDelivery } from "./utils";
+const OrderInfocontainer = ({ status = "failed" }) => {
   return (
     <Container>
       <div className="order-placed">
-        <CheckCircleFilled className="icon" height="29px" width="29px" />
+        {status == "failed" || status == "cancelled" ? (
+          <img src="https://d10srchmli830n.cloudfront.net/1653390909529_b6f7eb2a-0924-4d3b-961c-0e93a3e4ef33_Group-27444.svg" />
+        ) : (
+          <Icon
+            className="icon"
+            status={status}
+            style={{ color: Color(status) }}
+          />
+        )}
+
         <div className="content">
-          <div>Your Order has been placed</div>
+          <OrderStatus style={{ color: Color(status) }}>
+            {CheckOrderStatus(status)}
+          </OrderStatus>
+
           <div className="subcontent">
             Last updated on April 2nd 2022, 9:10 pm
           </div>
         </div>
       </div>
-      <div className="delivery-info">
-        <div>Expected Delivery </div>
-        <div>Arriving on Apr 06</div>
-      </div>
+      {status !== "delivered" && (
+        <div>
+          <div className="expected">Expected Delivery </div>
+          <div className="delivery-info">{ExpectedDelivery(status)}</div>
+        </div>
+      )}
       <div className="support">support@pickrr.com</div>
     </Container>
   );
--- app/components/OrderInfoContainer/style.js
+++ app/components/OrderInfoContainer/style.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { CheckCircleFilled } from "@ant-design/icons";
 export const Container = styled.div`
   background: #ffffff;
   border-radius: 8px;
@@ -6,18 +7,37 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  font-weight: 600;
+  font-style: normal;
+  color: #38446d;
   .order-placed {
     display: flex;
   }
-  .icon {
-    height: 29px;
-    width: 29px;
-  }
   .content {
-    color: #38446d;
-    font-weight: 600;
+    margin-left: 13px;
+    font-size: 18px;
     .subcontent {
       font-weight: 400;
+      font-size: 14px;
     }
   }
+  .expected {
+    font-size: 16px;
+  }
+  .delivery-info {
+    color: #158a2a;
+    font-weight: 700;
+    font-size: 14px;
+  }
+  .support {
+    font-size: 14px;
+    color: #1c439f;
+    font-weight: 400;
+  }
+`;
+
+export const OrderStatus = styled.div``;
+
+export const Icon = styled(CheckCircleFilled)`
+  font-size: 29px;
 `;
