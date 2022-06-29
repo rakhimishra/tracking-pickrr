624ebffc49d814e31cd51f53d02266cbf56455b7
Rakhi Mishra <rakhimishra@TECH-RAKHI.local>
2022-06-29 13:09:46 +0530

    remove actionfunction

--- app/utils/server.query.jsx
+++ app/utils/server.query.jsx
@@ -1,12 +1,12 @@
 // import { ActionFunction } from '@remix-run/node'; // or "@remix-run/cloudflare"
 import { redirect } from "@remix-run/node"; // or "@remix-run/cloudflare"
 
-// Note the "action" export name, this will handle our form POST
-export const ActionFunction = async ({ request }) => {
-  const trackingId = await request.formData();
-  //   const project = await getTrackingDetails(trackingId);
-  return redirect(`/tracking/${id}`);
-};
+// // Note the "action" export name, this will handle our form POST
+// export const ActionFunction = async ({ request }) => {
+//   const trackingId = await request.formData();
+//   //   const project = await getTrackingDetails(trackingId);
+//   return redirect(`/tracking/${id}`);
+// };
 
 export const getTrackingDetails = async (id) => {
   const data = await fetch(
